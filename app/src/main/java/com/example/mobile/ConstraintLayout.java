55ba3b66de96a2c452ccbef4afc84ead0608be75
Rohan Rajchal <rohanrajchal8@gmail.com>
2024-06-24 08:40:14 +0545

    Constraint ko fix garyeko tiyo

--- app/src/main/java/com/example/mobile/ConstraintLayout.java
+++ app/src/main/java/com/example/mobile/ConstraintLayout.java
@@ -1,5 +1,6 @@
 package com.example.mobile;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -10,6 +11,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ConstraintLayout extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
