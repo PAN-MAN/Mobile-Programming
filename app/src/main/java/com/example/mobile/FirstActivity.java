ed4cfc737ebcb566893883489aa56420e75b57de
Rohan Rajchal <rohanrajchal8@gmail.com>
2024-07-11 14:09:33 +0545

    Page Redirect

--- app/src/main/java/com/example/mobile/FirstActivity.java
+++ app/src/main/java/com/example/mobile/FirstActivity.java
@@ -4,39 +4,39 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class FirstActivity extends AppCompatActivity {
+
+    TextView txt;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first);
 
-        View txt = findViewById(R.id.text100);
-        View btn = findViewById(R.id.butt);
-        btn.setOnClickListener (new View.OnClickListener() {
+        txt = findViewById(R.id.text100);
+        btn = findViewById(R.id.butt);
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick (View view) {
-                Intent i=new Intent (FirstActivity.this,
-                        SecondActivity.class);
-                startActivityForResult (i,2);
-            }
-            @Override
-            protected void onActivityResult (int requestCode, int resultCode, Intent data)
-            {
-                super.onActivityResult (requestCode, resultCode, data);
-                if (requestCode==2)
-                {
-                    String message=data.getStringExtra ("message");
-                    txt.setText (message);
-                }
+            public void onClick(View view) {
+                Intent i = new Intent(FirstActivity.this, SecondActivity.class);
+               i.putExtra("name","This is First Activity");
+                startActivityForResult(i, 2);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 2 && resultCode == RESULT_OK) {
+            String message = data.getStringExtra("message");
+            System.out.println(message);
+            txt.setText(message);
+        }
     }
 }
--- app/src/main/java/com/example/mobile/SecondActivity.java
+++ app/src/main/java/com/example/mobile/SecondActivity.java
@@ -1,31 +1,37 @@
 package com.example.mobile;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.location.Address;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class SecondActivity extends AppCompatActivity {
+    Button btn;
+    TextView nameTextView; // Declare the TextView
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        Intent i=getIntent();
-        int id=i.getIntExtra("id",0);
-        String name=i.getStringExtra ("name");
-        String address=i.getStringExtra ("Address");
+        Intent i = getIntent();
+        String data = i.getStringExtra("name"); // Get the data from the intent
 
-//Displaying received data in TextView
-        TextView txt = findViewById(R.id.txt);
-        txt.setText ("Id="+id+"\n"+"Name="+name+"\n"+"Address="+ address) ;
+        nameTextView = findViewById(R.id.txt); // Initialize the TextView
+        nameTextView.setText(data); // Set the data as the text of the TextView
+
+        btn = findViewById(R.id.but);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent resultIntent = new Intent();
+                resultIntent.putExtra("message", "Hello First Activity!");
+                setResult(RESULT_OK, resultIntent);
+                finish();
+            }
+        });
     }
 }
